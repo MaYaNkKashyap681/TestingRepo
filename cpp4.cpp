7abf243df8f96d81592f1b251275e8325df77eb8
Mayank Wised <mayank@wisedinc.com>
2023-10-29 00:13:51 +0530

    Adding Code to print name and no of chars in it

---
+++ cpp4.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string name = "Mayank Kashyap";
+    cout<<"Name is: "<<name<<"\n";
+    cout<<"Charachters in the Name: "<<name.length()<<"\n";
+    return 0;
+}
